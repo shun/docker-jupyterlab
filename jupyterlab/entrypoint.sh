baa9624bd125cccc409abc034d0dc7763cb28743
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-08-01 05:37:45 +0000

    up

--- jupyterlab/entrypoint.sh
+++ jupyterlab/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/home/$WORKUSER/.local/bin:$PATH
-export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0:/home/jupyter/.local/lib/python3.8/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0
 
 echo "[$@]"
 exec "$@"
